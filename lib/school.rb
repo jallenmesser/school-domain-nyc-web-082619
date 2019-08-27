51d6a497acdcc13b48debd59d044b9fe177479c1
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-08-27 19:51:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,23 +4,19 @@ require "pry"
 class School
   attr_reader :school_name, :roster
   
+  ROSTER = {}
+  
   def initialize(school_name)
     @school_name = school_name
   end
   
-  def roster
-    @roster = { }
-  end
-  
-  #ROSTER = @roster
-  
   def add_student(name, grade)
     binding.pry
-    if @roster[grade]
-      @roster[grade] << name
+    if ROSTER[grade]
+      ROSTER[grade] << name
     else
-      @roster[grade] = []
-      @roster[grade] << name
+      ROSTER[grade] = []
+      ROSTER[grade] << name
     end
   end 
 end 
